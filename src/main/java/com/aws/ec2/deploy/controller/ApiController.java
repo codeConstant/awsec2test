a3bc58770b3b9f965e4a04eadab909898fb1745d
Arefin <arefin@bluetechbd.com>
2024-02-24 15:55:49 +0600

    commit the changes

---
+++ src/main/java/com/aws/ec2/deploy/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.aws.ec2.deploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("hello")
+    public String hello() {
+        return "My first docker container ...";
+    }
+}

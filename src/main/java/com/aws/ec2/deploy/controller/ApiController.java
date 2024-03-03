0cf6b5506a4a24eb3b8fb1c788bf49237b61b821
Arefin <arefin@bluetechbd.com>
2024-03-03 11:17:01 +0600

    run artifact service

--- src/main/java/com/aws/ec2/deploy/controller/ApiController.java
+++ src/main/java/com/aws/ec2/deploy/controller/ApiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
     @GetMapping("hello")
     public String hello() {
-        return "My first runner ...";
+        return "Hello, I am being printed from ec2 ...";
     }
 
 
